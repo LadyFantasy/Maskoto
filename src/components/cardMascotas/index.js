a852d07047d2bff184633bc602d73f4ad84e1b62
LuObreg <LuObreg@users.noreply.github.com>
2021-02-15 19:37:08 -0300

    estilo del btn refugio

--- src/components/cardMascotas/index.js
+++ src/components/cardMascotas/index.js
@@ -8,10 +8,10 @@ function CardMascotas({ data }) {
       <div>
         <h2 className="nombre">{data.nombre}</h2>
         <p>{data.status === "libre" ? "En adopción" : "Adoptado"}</p>
-         <p>        <a href={`https://hackathon-front.vercel.app/refugios#${data.refugio}`}>Refugio</a>
-      </p> 
         <p>{data.especie.charAt(0).toUpperCase() + data.especie.slice(1)}</p>
         <p>{data.sexo.charAt(0).toUpperCase() + data.sexo.slice(1)}</p>
+        <button className="bg-purple-400 hover:bg-indigo-500 text-white font-bold py-2 px-4 rounded m-2 lg:justify-center">        <a href={`https://hackathon-front.vercel.app/refugios#${data.refugio}`}>Refugio</a>
+      </button>
       </div>
     </div>
   );
