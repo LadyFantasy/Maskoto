1ad85ef62ed4a67b1dc08e6ba500195bcb1b18f0
LuObreg <LuObreg@users.noreply.github.com>
2021-02-15 19:13:58 -0300

    modif card refugios y link desde mascota

--- src/components/cardMascotas/index.js
+++ src/components/cardMascotas/index.js
@@ -3,15 +3,13 @@ import "../../index.css";
 function CardMascotas({ data }) {
   return (
     <div className="containerCard">
-      <img className="img" src={data.image} alt="perro o gato" />
+      <img className="img" src={data.image} alt={data.especie} />
 
       <div>
         <h2 className="nombre">{data.nombre}</h2>
         <p>{data.status === "libre" ? "En adopción" : "Adoptado"}</p>
-        {/* <p>
-        Refugio: (FALTA DATA DEL BACK)
-        <a href="">{data.refugio_id}</a>
-      </p> */}
+         <p>        <a href={`https://hackathon-front.vercel.app/refugios#${data.refugio}`}>Refugio</a>
+      </p> 
         <p>{data.especie.charAt(0).toUpperCase() + data.especie.slice(1)}</p>
         <p>{data.sexo.charAt(0).toUpperCase() + data.sexo.slice(1)}</p>
       </div>
--- src/components/cardRefugios/index.js
+++ src/components/cardRefugios/index.js
@@ -5,23 +5,24 @@ import "../../index.css"
 
 function cardRefugios({ data }) {
   return (
-    <div className="containerCard">
-      <img className="img" src={data.image} alt="imagen refugio"/>
-      <h2 className="nombre">{data.nombre}</h2>
-
-      <div className="containerLogoText">
-        <img style={{ width: "1.5rem" }} src={whatsappImg} alt="whatsapp" />
-        {data.whatsapp}
+    <div className="containerCardSingle" id={data._id}>
+      <img className="imgSingle" src={data.image} alt="imagen refugio"/>
+      <div className="containerInfo">
+        <h2 className="nombre">{data.nombre}</h2>
+        <div className="containerLogoText">
+          <img style={{ width: "1.5rem" }} src={whatsappImg} alt="whatsapp" />
+          {data.whatsapp}
+        </div>
+        <p>Dirección: {data.direccion}</p>
+        <p>Ciudad: {data.ciudad}</p>
+        <div className="containerLogoText">
+          <img style={{ width: "2rem" }} src={instagramImg} alt="instagram" />
+          <a href={data.instagram}>{data.nombre}</a>
+        </div>
+        <a href={data.url_donar}>
+          <img style={{ width: "3rem" }} src={mercadoPagoImg} alt="mercadopago" />
+        </a>
       </div>
-      <p>Dirección: {data.direccion}</p>
-      <p>Ciudad: {data.ciudad}</p>
-      <div className="containerLogoText">
-        <img style={{ width: "2rem" }} src={instagramImg} alt="instagram" />
-        <a href={data.instagram}>{data.nombre}</a>
-      </div>
-      <a href={data.url_donar}>
-        <img style={{ width: "3rem" }} src={mercadoPagoImg} alt="mercadopago" />
-      </a>
     </div>
   );
 }
