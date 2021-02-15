6a40de888cf74d7e0e5f08d30152d065321562f8
LadyFantasy <msoledadm88@gmail.com>
2021-02-15 20:03:40 +0000

    borré títulos

--- src/pages/Mascotas/index.js
+++ src/pages/Mascotas/index.js
@@ -4,7 +4,7 @@ import Header from "../../components/Header/index.js";
 import Footer from "../../components/Footer/index";
 import CardMascotas from "../../components/cardMascotas";
 import Button from "../../components/Button";
-import "../../index.css"
+import "../../index.css";
 
 function Mascotas() {
   const url = "https://hackourseit.herokuapp.com/mascotas";
@@ -23,7 +23,7 @@ function Mascotas() {
   return (
     <>
       <Header />
-      <h1 class="mb-8 text-2xl font-bold tracking-tighter text-center text-blue-800 lg:text-left lg:text-5xl title-font">Mascotas en adopción</h1>
+
       <div style={{ display: "flex", flexDirection: "row", justifyContent: "center" }}>
         <Button data={"Perros"} link={true} />
         <Button data={"Gatos"} link={true} />
--- src/pages/Refugios/index.js
+++ src/pages/Refugios/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import Header from "../../components/Header/index.js";
 import CardRefugios from "../../components/cardRefugios";
 import Button from "../../components/Button";
-import "../../index.css"
+import "../../index.css";
 
 function Refugios() {
   const url = "https://hackourseit.herokuapp.com/refugios";
@@ -22,7 +22,6 @@ function Refugios() {
   return (
     <>
       <Header></Header>
-      <h1 class="mb-8 text-2xl font-bold tracking-tighter text-center text-blue-800 lg:text-left lg:text-5xl title-font">Refugios:</h1>
       <div className="containerPage">
         {refugios.map((item, key) => {
           return <CardRefugios key={key} data={item} />;
